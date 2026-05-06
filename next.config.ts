67b96f1a18fe8b016bf0e0a0d9ad7e9ebf68ae82
MylittleQueercat <2628938535ghl@gmail.com>
2026-05-06 09:53:09 +0200

    rm middleware

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import { withIntlayer } from "next-intlayer/server";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/coming-soon',
+        destination: '/fr/coming-soon',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default withIntlayer(nextConfig);
